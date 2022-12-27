1f384d0cf84a6ecbdd274f5b5a2cc7dd58852191
aco4 <anthonythegu@gmail.com>
2022-12-27 15:24:48 -0500

    Non-static chunk scan

--- src/main/java/com/gmail/anthonythegu/terrainanalyzer/CommandAnalyzeTerrain.java
+++ src/main/java/com/gmail/anthonythegu/terrainanalyzer/CommandAnalyzeTerrain.java
@@ -16,74 +16,79 @@ public class CommandAnalyzeTerrain implements CommandExecutor {
             Player p = (Player) sender;
 
             if (args.length == 0) {
-                sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain (chunk|scan) [ignore]");
+                sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain (thischunk|loadedchunks) [ignore]");
                 return false;
             }
 
             if (args.length == 1) {
-                if (args[0].toLowerCase().equals("chunk")) { // analyzeterrain chunk
-                    Statistics s = TerrainScanner.scanChunk(p, "");
-                    p.sendMessage(prefix() + " The average height of this chunk is " + s.average() + " blocks");
-                    p.sendMessage(prefix() + " The mode height of this chunk is " + s.mode() + " blocks");
+                if (args[0].toLowerCase().equals("thischunk")) { // EX: analyzeterrain thischunk
+
+                    TerrainScanner ts = new TerrainScanner();
+                    ts.scanChunk(p.getLocation().getChunk(), "");
+
+                    p.sendMessage(prefix() + " The average height of this chunk is " + ts.average() + " blocks");
+                    p.sendMessage(prefix() + " The mode height of this chunk is " + ts.mode() + " blocks");
                     return true;
-                } else if (args[0].toLowerCase().equals("scan")) { // analyzeterrain scan
-                    sender.sendMessage(
-                            ChatColor.RED + "ERROR: This command is in beta and has not been implemented yet!");
-                    return false;
-                } else { // analyzeterrain blah
-                    sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain (chunk|scan) [ignore]");
+
+                } else if (args[0].toLowerCase().equals("loadedchunks")) { // EX: analyzeterrain loadedchunks
+
+                    TerrainScanner ts = new TerrainScanner();
+                    ts.scanLoadedChunks(p.getWorld(), "");
+
+                    p.sendMessage(prefix() + " The average height of all loaded chunks in this world is " + ts.average()
+                            + " blocks");
+                    p.sendMessage(prefix() + " The mode height of all loaded chunks in this world is " + ts.mode()
+                            + " blocks");
+                    return true;
+
+                } else { // EX: analyzeterrain blah
+                    sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain (thischunk|loadedchunks) [ignore]");
                     return false;
                 }
             }
 
-            if (args.length == 2) {
-                if (args[0].toLowerCase().equals("chunk")) {
-                    if (args[1].toLowerCase().equals("ignore")) { // analyzeterrain chunk ignore
-                        sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain chunk ignore <blocks>");
-                        return false;
-                    } else { // analyzeterrain chunk blah
-                        sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain chunk [ignore] <blocks>");
-                        return false;
-                    }
-                } else { // analyzeterrain blah blah
-                    sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain (chunk|scan) [ignore]");
-                    return false;
-                }
+            if (args.length == 2) {  // EX: analyzeterrain thischunk ignore
+                sender.sendMessage(
+                        ChatColor.RED + "Usage: /analyzeterrain (thischunk|loadedchunks) [ignore] [<blocks>]");
+                return false;
             }
 
             if (args.length == 3) {
-                if (args[0].toLowerCase().equals("chunk")) {
-                    if (args[1].toLowerCase().equals("ignore")) { // analyzeterrain chunk ignore stone
-                        Statistics s = TerrainScanner.scanChunk(p, args[2]);
-                        p.sendMessage(prefix() + " The average height of this chunk is " + s.average() + " blocks");
-                        p.sendMessage(prefix() + " The mode height of this chunk is " + s.mode() + " blocks");
-                        return true;
-                    } else { // analyzeterrain chunk blah blah
-                        sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain chunk [ignore] <blocks>");
-                        return false;
-                    }
-                } else { // analyzeterrain blah blah blah
-                    sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain (chunk|scan) [ignore]");
+                if (args[0].toLowerCase().equals("thischunk") && args[1].toLowerCase().equals("ignore")) {
+
+                    TerrainScanner ts = new TerrainScanner();
+                    ts.scanChunk(p.getLocation().getChunk(), args[2]);
+
+                    p.sendMessage(prefix() + " The average height of this chunk is " + ts.average() + " blocks");
+                    p.sendMessage(prefix() + " The mode height of this chunk is " + ts.mode() + " blocks");
+                    return true;
+
+                } else if (args[0].toLowerCase().equals("loadedchunks") && args[1].toLowerCase().equals("ignore")) {
+
+                    TerrainScanner ts = new TerrainScanner();
+                    ts.scanLoadedChunks(p.getWorld(), args[2]);
+
+                    p.sendMessage(
+                            prefix() + " The average height of all loaded chunks in this world is " + ts.average()
+                                    + " blocks");
+                    p.sendMessage(prefix() + " The mode height of all loaded chunks in this world is " + ts.mode()
+                            + " blocks");
+                    return true;
+
+                } else { // EX: analyzeterrain blah ignore stone
+                    sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain (thischunk|loadedchunks) ignore <blocks>");
                     return false;
                 }
             }
 
-            if (args.length >= 4) {
-                if (args[0].toLowerCase().equals("chunk")) {
-                    if (args[1].toLowerCase().equals("ignore")) { // analyzeterrain chunk ignore stone water
-                        sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain chunk ignore BLOCK1,BLOCK2...");
-                        return false;
-                    } else { // analyzeterrain chunk blah blah blah
-                        sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain chunk [ignore] <blocks>");
-                        return false;
-                    }
-                } else { // analyzeterrain blah blah blah blah
-                    sender.sendMessage(ChatColor.RED + "Usage: /analyzeterrain (chunk|scan) [ignore]");
-                    return false;
-                }
+            if (args.length >= 4) { // EX: analyzeterrain thischunk ignore stone air water
+                sender.sendMessage(
+                        ChatColor.RED + "Usage: /analyzeterrain (thischunk|loadedchunks) ignore BLOCK1,BLOCK2...");
+                return false;
             }
         }
         return false;
+
     }
 
     private static String prefix() {
--- src/main/java/com/gmail/anthonythegu/terrainanalyzer/TerrainScanner.java
+++ src/main/java/com/gmail/anthonythegu/terrainanalyzer/TerrainScanner.java
@@ -7,40 +7,65 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 
 public class TerrainScanner {
     private static final int WORLD_UPPER = 319;
     private static final int WORLD_LOWER = -64;
 
-    // private boolean registerChunk(Chunk chunk) {
-    // if (!scannedChunks.contains(chunk)) {
-    // scannedChunks.add(chunk);
-    // return true;
-    // }
-    // return false;
-    // }
+    private ArrayList<Chunk> scannedChunks;
 
-    public static Statistics scanChunk(Chunk chunk, String ignored) {
+    private RollingAverage rollingAverage;
+    private RollingMode rollingMode;
 
-        Statistics statistics = new Statistics();
-        ArrayList<Material> filter = toMaterials(ignored);
-        int cx = 16 * chunk.getX();
-        int cz = 16 * chunk.getZ();
-
-        for (int x = cx; x < cx + 16; x++) {
-            for (int z = cz; z < cz + 16; z++) {
-                Location location = new Location(chunk.getWorld(), x, 0, z);
-
-                int highestY = getTerrainHeight(location, filter);
-                statistics.add(highestY);
-            }
-        }
-        return statistics;
+    public TerrainScanner() {
+        scannedChunks = new ArrayList<>();
+        rollingAverage = new RollingAverage();
+        rollingMode = new RollingMode();
     }
 
-    public static Statistics scanChunk(Player player, String ignored) {
-        return scanChunk(player.getLocation().getChunk(), ignored);
+    public void add(int num) {
+        rollingAverage.add(num);
+        rollingMode.add(num);
+    }
+
+    public int average() {
+        return rollingAverage.get();
+    }
+
+    public int mode() {
+        return rollingMode.get();
+    }
+
+    private boolean registerChunk(Chunk chunk) {
+        if (!scannedChunks.contains(chunk)) {
+            scannedChunks.add(chunk);
+            return true;
+        }
+        return false;
+    }
+
+    public void scanLoadedChunks(World world, String ignored) {
+        Chunk[] loadedChunks = world.getLoadedChunks();
+        for (Chunk c : loadedChunks) {
+            scanChunk(c, ignored);
+        }
+    }
+
+    public void scanChunk(Chunk chunk, String ignored) {
+        if (!scannedChunks.contains(chunk)) {
+            registerChunk(chunk);
+            ArrayList<Material> filter = toMaterials(ignored);
+            World w = chunk.getWorld();
+            int cx = 16 * chunk.getX();
+            int cz = 16 * chunk.getZ();
+
+            for (int x = cx; x < cx + 16; x++) {
+                for (int z = cz; z < cz + 16; z++) {
+                    Location location = new Location(w, x, 0, z);
+                    add(getTerrainHeight(location, filter));
+                }
+            }
+        }
     }
 
     private static int getTerrainHeight(Location location, ArrayList<Material> ignored) {
